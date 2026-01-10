35cfc86b9fa848f53cd2f613a8ae7ea795d99684
Tushar Upadhyay <tusharupadhyay628@gmail.com>
2026-01-10 18:25:04 +0300

    first commit

---
+++ BASICS/07_conditional.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 45;
+    // conditional statement to check if n is positive or negative
+    if (n >= 0){
+        cout << n << " is a positive number." << endl;
+    } else {
+        cout << n << " is a negative number." << endl;
+    }
+    return 0;
+}
