9efbf8a85a889cb60320b1fe492ed713515d9194
Tushar Upadhyay <tusharupadhyay628@gmail.com>
2026-01-11 10:59:40 +0300

    morning!!!!!!!!!!!

---
+++ BASICS/10_for.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 5; ++i) { // for(initialization; condition; increment)
+        std::cout << "Loop iteration: " << i << std::endl;
+    }
+    return 0;
+}
